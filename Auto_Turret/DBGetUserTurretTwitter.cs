6bb25e587b7c4dafb80bdce8556ea6198732c33d
Brent Rademaker <brademaker@live.com>
2017-06-13 22:39:57 -0400

    barebones work on the auto turret computer app
    don't actually have the hardware to work on a lot of these methods so working on the app pulling from the database. ran into an issue with unit testing

---
+++ Auto_Turret/DBGetUserTurretTwitter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Auto_Turret
+{
+    class DBGetUserTurretTwitter
+    {
+        public DBGetUserTurretTwitter(SqlConnection conncetion, int user_id, int turret_id, string twitter)
+        {
+
+        }
+    }
+}

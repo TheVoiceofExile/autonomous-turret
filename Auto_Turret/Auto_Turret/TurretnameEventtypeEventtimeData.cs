a42f9209342e60a26ea51dfaf57db79abb1dcb26
Brent Rademaker <brademaker@live.com>
2017-06-22 08:57:33 -0400

    Added class to contain a query specific db query
    this class is purely a data type that holds the name of a turret and its
    associated event types and times

---
+++ Auto_Turret/Auto_Turret/TurretnameEventtypeEventtimeData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_Turret
+{
+    class TurretnameEventtypeEventtimeData
+    {
+        public string TurretName;
+        public string EventType;
+        public DateTime EventTime;
+
+        public TurretnameEventtypeEventtimeData(string TurretName, string EventType, DateTime EventTime)
+        {
+            this.TurretName = TurretName;
+            this.EventType = EventType;
+            this.EventTime = EventTime;
+        }
+    }
+}

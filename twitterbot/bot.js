807eb210cf5983aa704230bf29d341e0c748e089
raforero <forero.ra@gmail.com>
2017-06-22 21:44:40 -0400

    added twit package and associated modules

---
+++ twitterbot/bot.js
@@ -0,0 +1,11 @@
+console.log("The bot is working.");
+//################################################
+
+const Twit = require('twit');
+
+const T = new Twit({
+    consumer_key:           '...',
+    consumer_secret:        '...',
+    access_token:           '...',
+    access_token_secret:    '...'
+})

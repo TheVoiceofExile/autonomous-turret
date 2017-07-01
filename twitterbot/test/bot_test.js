fa1f27172560dc016aa68e5a502f16def3595f85
raforero <forero.ra@gmail.com>
2017-07-01 17:42:30 -0400

    Began unit testing for bot.js

---
+++ twitterbot/test/bot_test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+
+// TODO: export functions from bot.js
+const bot = require('../bot'); 
+
+describe('bot', function () {
+    it('should <do something>', function () {
+        // Test statement (assertion)
+    });
+});

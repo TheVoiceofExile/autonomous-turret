b303a4fb3f9b6d6fbb83f0e6364eec4486f4116c
raforero <forero.ra@gmail.com>
2017-07-15 19:45:13 -0400

    queried data and sent it in a direct message

--- twitterbot/bot.js
+++ twitterbot/bot.js
@@ -5,10 +5,14 @@ const Request = require('tedious').Request;
 
 console.log('BOT ACTIVATED.');
 
-/*
- * CONNECT TO AZURE DATABASE
- */
-const config = {
+const twitterbot = new Twit({
+    consumer_key: 'BLMFmhRGVSbimKjFcvhKj28jL',
+    consumer_secret: 'FWmV1rRWWIGBnrzfNQ2Uz2zRsJVTl73HGPD8qGdlxHb7nE6FLt',
+    access_token: '878069495037919232-38XczPn1cBOBJmnQJIHLp5EqQCvZDGW',
+    access_token_secret: 'tH4KDsmoXJgXZSURkHoaeNOu1JbsiGQajyCiclUeKn2y4',
+});
+
+const connection = new Connection({
     userName: 'TwitterLogin',
     password: 'Twitterbot8*',
     server: 'softdev.database.windows.net',
@@ -17,102 +21,59 @@ const config = {
         encrypt: true,
         database: 'AutoTurret',
     }
-}
-
-const connection = new Connection(config);
+});
 
 connection.on('connect', function (err) {
     if (err) {
         console.log(err);
     } else {
-        console.log('Connected to database.');
+        console.log('Connected to database.')
         queryDatabase();
-        // Format data
-        // Post Twitter dm
     }
 });
 
-/*
- * QUERY DATA
- */
-function queryDatabase() { 
-    console.log('Reading rows from the Table...');
+const queryDatabase = function () { 
+    console.log('Reading rows from table...');
 
-    // Read all rows from table
     request = new Request(
-        // Number of events a turret has
         "SELECT COUNT(*) " + 
         "FROM dbo.Turrets, dbo.Events " + 
-        "WHERE Turrets.turret_id=Events.fk_turret_id ",
+        "WHERE Turrets.turret_id = Events.fk_turret_id ",
  
-        function(err, rowCount, rows) {
+        function (err, rowCount, rows) {
             if (err) {
                 console.log(err);
             } else {
                 console.log(rowCount + ' row(s) returned');
-                process.exit();
             }
         }
     );
 
     request.on('row', function (columns) {
         columns.forEach(function (column) {
-                console.log("%s\t%s", column.metadata.colName, column.value);
+            console.log('Number of events in turret: %s\t%s', column.metadata.colName, column.value);
+            postDirectMessage(column.value);
         });
     });
 
     connection.execSql(request);
 }
 
-/*
- * CONNECT TO TWITTER API
- */
-const T = new Twit({
-    consumer_key: 'BLMFmhRGVSbimKjFcvhKj28jL',
-    consumer_secret: 'FWmV1rRWWIGBnrzfNQ2Uz2zRsJVTl73HGPD8qGdlxHb7nE6FLt',
-    access_token: '878069495037919232-38XczPn1cBOBJmnQJIHLp5EqQCvZDGW',
-    access_token_secret: 'tH4KDsmoXJgXZSURkHoaeNOu1JbsiGQajyCiclUeKn2y4',
-});
+const postDirectMessage = function (queryOutput) {
+    const numEvents = queryOutput;
 
-/*
- * POST DIRECT MESSAGE
- */
-/*T.post(
-    'direct_messages/new', {
-        user_id: '2889985308', // @ra_forero
-        text: emoji.emojify('Hello I am Bot :hand: :robot_face:'),
-    }, 
-    function (err, data, response) {
-        console.log('Message sent to @ra_forero.');
-    }
-);
-
-T.post(
-    'direct_messages/new', {
-        user_id: '879129222618513408', // @brent_rademaker
-        text: emoji.emojify('Hello I am Bot :hand: :robot_face:'),
-    }, 
-    function (err, data, response) {
-        console.log('Message sent to @brent_rademaker.');
-    }
-);
-
-T.post(
-    'direct_messages/new', {
-        user_id: '3622865902', // @billy_bil88
-        text: emoji.emojify('Hello I am Bot :hand: :robot_face:'),
-    }, 
-    function (err, data, response) {
-        console.log('Message sent to @billy_bil88.');
-    }
-);
-
-T.post(
-    'direct_messages/new', {
-        user_id: '883285308', // @megamatt119
-        text: emoji.emojify('Hello I am Bot :hand: :robot_face:'),
-    }, 
-    function (err, data, response) {
-        console.log('Message sent to @megamatt119.');
-    }
-);*/
+    twitterbot.post(
+        'direct_messages/new', {
+            user_id: '2889985308', // @ra_forero
+            text: emoji.emojify(`${numEvents} events in turret :hand: :robot_face:`),
+        }, 
+        function (err, data, response) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Direct message sent to @ra_forero.');
+                process.exit();
+            }
+        }
+    );
+}
